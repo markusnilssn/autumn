9ecbdd0cfd718652faa497b6524ef3ea2e6a5c59
Markus Nilsson <71263532+markusnilssn@users.noreply.github.com>
2023-04-02 12:13:15 +0200

    Add Empty interface

--- Source/Engine/Engine/Graphics/Shader.h
+++ Source/Engine/Engine/Graphics/Shader.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 
-// #include <GLAD/glad.h>
-// #include <glm/glm.hpp>
-// #include <glm/gtc/type_ptr.hpp>
+//#include <GLAD/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Autumn
 {
@@ -11,26 +11,26 @@ namespace Autumn
     {
         class Shader
         {
-        //     unsigned int ID;
-        //     Shader();
-        // 
-        //     Shader& Use();
-        //     // compiles the shader from given source code
-        //     void Compile(const char* vertexSource, const char* fragmentSource, const char* geometrySource = nullptr); // note: geometry source code is optional 
-        //     // utility functions
-        //     void SetFloat(const char* name, float value, bool useShader = false);
-        //     void SetInteger(const char* name, int value, bool useShader = false);
-        //     void SetVector2f(const char* name, float x, float y, bool useShader = false);
-        //     void SetVector2f(const char* name, const glm::vec2& value, bool useShader = false);
-        //     void SetVector3f(const char* name, float x, float y, float z, bool useShader = false);
-        //     void SetVector3f(const char* name, const glm::vec3& value, bool useShader = false);
-        //     void SetVector4f(const char* name, float x, float y, float z, float w, bool useShader = false);
-        //     void SetVector4f(const char* name, const glm::vec4& value, bool useShader = false);
-        //     void SetMatrix4(const char* name, const glm::mat4& matrix, bool useShader = false);
-        // 
-        // private:
-        //     void checkCompileErrors(unsigned int object, std::string type);
-        // 
+             unsigned int ID;
+             Shader();
+         
+             Shader& Use();
+             // compiles the shader from given source code
+             void Compile(const char* vertexSource, const char* fragmentSource, const char* geometrySource = nullptr); // note: geometry source code is optional 
+             // utility functions
+             void SetFloat(const char* name, float value, bool useShader = false);
+             void SetInteger(const char* name, int value, bool useShader = false);
+             void SetVector2f(const char* name, float x, float y, bool useShader = false);
+             void SetVector2f(const char* name, const glm::vec2& value, bool useShader = false);
+             void SetVector3f(const char* name, float x, float y, float z, bool useShader = false);
+             void SetVector3f(const char* name, const glm::vec3& value, bool useShader = false);
+             void SetVector4f(const char* name, float x, float y, float z, float w, bool useShader = false);
+             void SetVector4f(const char* name, const glm::vec4& value, bool useShader = false);
+             void SetMatrix4(const char* name, const glm::mat4& matrix, bool useShader = false);
+         
+         private:
+             void checkCompileErrors(unsigned int object, std::string type);
+         
         };
     }
 }
